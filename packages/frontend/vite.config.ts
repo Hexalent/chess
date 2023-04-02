ba8bf66891285c830dda0a6f438fc3debddd66cf
Max Yakushev <m.yakushev23@gmail.com>
2023-04-02 16:17:43 +0300

    feat(utils): configure frontend utilities package

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,17 +1,16 @@
-import * as path from "node:path";
+import react from '@vitejs/plugin-react'
+import * as path from 'node:path'
+import { packageDirectorySync } from 'pkg-dir'
+import { defineConfig } from 'vite'
 
-import { packageDirectorySync } from "pkg-dir";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-const packageRoot = packageDirectorySync();
+const packageRoot = packageDirectorySync()
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      src: path.resolve(packageRoot, "./src"),
-    },
-  },
-});
+      src: path.resolve(packageRoot, './src')
+    }
+  }
+})
